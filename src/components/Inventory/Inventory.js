416a227efa90225a4193997db7e24b055dab6858
Nahid51 <hasannahidnahid@gmail.com>
2021-09-30 22:47:56 +0600

    create inventory page

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            <h2>Create new invetory page</h2>
+        </div>
+    );
+};
+
+export default Inventory;
